23f437516c5ce469a3b4784978a73958c3a940d7
Xavier Meckler <xmeckler@gmail.com>
2018-05-09 16:37:38 +0200

    Creation de ListingController

---
+++ src/AppBundle/Controller/ListingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use AppBundle\Entity\Flight;
+use AppBundle\Entity\PlaneModel;
+use AppBundle\Entity\Reservation;
+
+class ListingController extends Controller
+{
+}
