8791f9c98dcaddc83787fa265bf4c7a2bb31adc0
Xavier Meckler <xmeckler@gmail.com>
2018-05-09 16:52:10 +0200

    Ajout de la méthode indexAction à ListingController

--- src/AppBundle/Controller/ListingController.php
+++ src/AppBundle/Controller/ListingController.php
@@ -11,6 +11,19 @@ use AppBundle\Entity\Flight;
 use AppBundle\Entity\PlaneModel;
 use AppBundle\Entity\Reservation;
 
+/**
+ * Listing controller.
+ *
+ * @Route("listing")
+ */
 class ListingController extends Controller
 {
+    public function indexAction(Reservation $reservation, Flight $flight, PlaneModel $planemodel)
+    {
+        return $this->render('listing/index.html.twig', array(
+            'reservation' => $reservation,
+            'flight' => $flight,
+            'planemodel' => $planemodel
+        ));
+    }
 }
