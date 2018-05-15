af57319be552e3a62fb22ddc76f1cec9fbac9bb6
Xavier Meckler <xmeckler@gmail.com>
2018-05-15 20:09:53 +0200

    creation de review showAction et sa vue

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -7,8 +7,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
+
 use AppBundle\Entity\Review;
-use AppBundle\Entity\User;
+use AppBundle\Form\ReviewType;
 
 /**
  * Review controller.
@@ -40,12 +41,60 @@ class ReviewController extends Controller
      * @Route("/new", name="review_new")
      * @Method({"GET", "POST"})
      */
-    public function newAction()
+    public function newAction(\Symfony\Component\HttpFoundation\Request $request)
     {
         $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review);
+        // build the form ...
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            // You can use too :
+            // return $this->redirect($this->generateUrl('review_show', array('id' => $review->getId())))
+
+            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+        }
+
         return $this->render('review/new.html.twig', array(
             'review' => $review,
+            'form' => $form->createView(),
         ));
     }
 
+    /**
+     * Finds and displays a review entity.
+     *
+     * @Route("/{id}", name="review_show")
+     * @Method("GET")
+     */
+    public function showAction(Review $review)
+    {
+        $deleteForm = $this->createDeleteForm($review);
+
+        return $this->render('review/show', array(
+            'review' => $review,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to delete a review entity.
+     *
+     * @param Review $review The review entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Review $review)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('review_delete', array('id' => $review->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
+    }
 }
--- src/AppBundle/Form/ReviewType.php
+++ src/AppBundle/Form/ReviewType.php
@@ -9,7 +9,16 @@
 namespace AppBundle\Form;
 
 
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReviewType extends AbstractType
 {
@@ -19,10 +28,17 @@ class ReviewType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('text')
-            ->add('publicationDate')
-            ->add('note')
-            ->add('userRated')
+            ->add('text', TextareaType::class, array('attr' => array('maxlength' => 250, 'label' => 'Description')))
+            ->add('publicationDate', DateType::class, array('data' => new \DateTime('now')))
+            ->add('note', IntegerType::class, array('attr' => array('min' => 0, 'max' => 5, 'label' => 'Note')))
+            ->add('agreeTerms', CheckboxType::class, array('mapped' => false))
+            ->add('userRated', EntityType::class, array(
+                'class' => 'AppBundle\Entity\User',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.lastName', 'ASC');
+                },
+                'choice_label' => 'lastName'))
             ->add('reviewAuthor');
     }
 
