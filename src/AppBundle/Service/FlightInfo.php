d2f68286c4471c20669471eb6383281f1eda5294
Xavier Meckler <xmeckler@gmail.com>
2018-05-27 11:08:25 +0200

    creation de Service/FlightInfo et de la méthode getDistance

---
+++ src/AppBundle/Service/FlightInfo.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder17
+ * Date: 27/05/18
+ * Time: 10:40
+ */
+
+namespace AppBundle\Service;
+
+
+class FlightInfo
+{
+    /**
+     * @var string
+     */
+    private $unit;
+
+    /**
+     * Constructor
+     *
+     * @param string $unit Defined in config.yml
+     */
+    public function __construct($unit)
+    {
+        $this->unit = $unit;
+    }
+
+    /**
+     * Distance calculation between latitude/longitude based on Harnive's formula
+     * http://www.codecodex.com/wiki/Calculate_Distance_Between_Two_Points_on_a_Globe#PHP
+     *
+     * @param float $latitudeFrom  Departure
+     * @param float $longitudeFrom Departure
+     * @param float $latitudeTo    Arrival
+     * @param float $longitudeTo   Arrival
+     *
+     * @return float
+     */
+    public function getDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo)
+    {
+        $d = 0;
+        $earth_radius = 6371;
+        $dLat = deg2rad($latitudeTo - $latitudeFrom);
+        $dLon = deg2rad($longitudeTo - $longitudeFrom);
+
+        $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * sin($dLon/2) * sin($dLon/2);
+        $c = 2 * asin(sqrt($a));
+
+        switch ($this->_unit) {
+            case 'km':
+                $d = $c * $earth_radius;
+                break;
+            case 'mi':
+                $d = $c * $earth_radius / 1.609344;
+                break;
+            case 'nmi':
+                $d = $c * $earth_radius / 1.852;
+                break;
+        }
+
+        return $d;
+    }
+}
